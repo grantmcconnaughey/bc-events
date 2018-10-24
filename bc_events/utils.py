8eeb7c5e9e0ed29822c19988f9997b82300750ec
Jonathan Findley <jon.f@technologist.com>
2018-10-24 13:58:38 -0500

    Remove get_requests_session

--- bc_events/utils.py
+++ bc_events/utils.py
@@ -1,8 +1,3 @@
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-
-
 def build_topic_name(category, entity, action):
     """Builds a topic name from it's parts
 
@@ -24,19 +19,3 @@ def build_topic_name(category, entity, action):
     """
     return "{category}.{entity}{action}".format(category=category, entity=entity, action=action)
 
-
-def get_requests_session():
-    """For use on AWS APIs that periodically fail with 500 and suggest retrying"""
-    session = requests.Session()
-    retry = Retry(
-        total=3, read=3, connect=3, status_forcelist=[500],
-        backoff_factor=0.3, method_whitelist=False
-    )
-    adapter = HTTPAdapter(
-        max_retries=retry,
-        pool_maxsize=20,
-        pool_connections=20
-    )
-    session.mount("https://", adapter)
-    session.mount("http://", adapter)
-    return session
