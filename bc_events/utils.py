48c5c3a1374304ff79089f3c529ea7b86848a458
Jonathan Findley <jon.f@technologist.com>
2018-10-23 19:41:33 -0500

    Add get_requests_session to give a requests session with HTTPAdapter capable of retrying on 500s

--- bc_events/utils.py
+++ bc_events/utils.py
@@ -1,3 +1,8 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
 def build_topic_name(category, entity, action):
     """Builds a topic name from it's parts
 
@@ -18,3 +23,20 @@ def build_topic_name(category, entity, action):
         Formatted topic name
     """
     return "{category}.{entity}{action}".format(category=category, entity=entity, action=action)
+
+
+def get_requests_session():
+    """For use on AWS APIs that periodically fail with 500 and suggest retrying"""
+    session = requests.Session()
+    retry = Retry(
+        total=3, read=3, connect=3, status_forcelist=[500],
+        backoff_factor=0.3, method_whitelist=False
+    )
+    adapter = HTTPAdapter(
+        max_retries=retry,
+        pool_maxsize=20,
+        pool_connections=20
+    )
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+    return session
